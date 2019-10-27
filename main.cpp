a11b38d8277cf91031c9756a61c6399de9299e6a
@Maus_74 <nikita.potapovich@mail.ru>
2019-10-27 23:15:15 +0300

    1 commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
+#include<any>
+#include<vector>
+typedef long long ll;
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ll n;
+    cin>>n;
+    vector<ll> A(n);
+
+    for(int i = 0; i < n;i++) {
+        cin>>A[i];
+    }
+
     return 0;
 }
